25788062e93545eb0f84098f3ac5d8346af4a302
Mutisostud02 <mutiso8395@gmail.com>
2022-12-27 15:31:07 +0300

    added more functions for fluidity added a function to generate the div squares when a user clicks the button and inputs a number edited the user input to return the square of the given number instead of the square root the grid generation function will now be generated inside the entersquare function the grid squares have been reduced in size the mouse over event now generates random colors as edited in the changeColor function edited the 'CLICK HERE'  button to be 'INSERT' added the 'INSERT' button to a buttonholder div added a new 'RESET' button to the buttonholder div gave the two buttons same styles added an event listener to the 'INSERT' button which has entersquare as callback function

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
-const container=document.createElement("div");
-container.classList.add('container');
+const emptyDiv=document.createElement('div');//empty div
+document.body.appendChild(emptyDiv);
+emptyDiv.classList.add('empt');
+emptyDiv.setAttribute('style','width:100%;');
 
-for(let i=1;i<257;i++){
-    const div=document.createElement("div");
-    div.classList.add("box");
-    div.classList.add(`square-${i}`);
-    container.appendChild(div);
-    
 
-}
-document.body.appendChild(container);
-const div=document.querySelectorAll('.box');
+const container=document.createElement("div");//create a new div to contain the grid squares
+container.classList.add('container');//gave the div a class
 
-function changeColor(element){
-    element.setAttribute('style','background-color:aqua;');
-}
+document.body.appendChild(container);//adding our container to the div
 
-div.forEach(element => {
-    element.addEventListener('mouseover',()=>{
-        changeColor(element);
-    }
 
-    );
-    
-});
-const btn=document.createElement('button');
-btn.textContent="CLICK HERE";
-document.body.insertBefore(btn,container);
-btn.setAttribute('style','margin:10px 40% 50px;width:200px;height:80px;')
 
+
+
+
+
+//function to take and return a number to be squared
 function enterSquare(){
-   let userInput=prompt('ENTER A SQUARE VALUE')
-   return console.log(userInput);
+    let userInput=prompt('ENTER A SQUARE VALUE');
+    
+    noOfSquare(userInput);
+    
 }
 
-btn.addEventListener('click',enterSquare);
+function noOfSquare(number){
+    container.setAttribute('style',`display:grid; grid-template-columns:repeat(${number},10px); grid-template-rows:repeat(${number},10px);`);
+    for(let i=0;i<(number*number);i++){
+        const divSquare=document.createElement('div');
+        divSquare.classList.add('box');
+        
+        container.appendChild(divSquare);
+        divSquare.setAttribute('style','border:0.01px solid grey;');
+        
+            divSquare.addEventListener('mouseover',()=>{
+                changeColor(divSquare);
+            }); 
+          
+                 
+    }    
+}
+
+
+const divSquare=document.querySelectorAll('.box');//selecting our div squares
+
+//function to change element color
+function changeColor(element){
+    let red=Math.floor(Math.random()*256);
+    let green=Math.floor(Math.random()*256);
+    let blue=Math.floor(Math.random()*256);
+    element.setAttribute('style',`background-color:rgb(${red},${green},${blue});`);
+}
+//function to return color on mouse leave
+function returnColor(element){
+    element.setAttribute('style','background-color:;border:1px black solid;')
+}
+
+const btnHolder=document.createElement('div');
+btnHolder.setAttribute('style','height:100px;width:300px;margin:auto;padding:auto;display:inline-block;');
+document.body.insertBefore(btnHolder,emptyDiv);
+
+const rstBtn=document.createElement('button');
+rstBtn.setAttribute('style','width:80px;height:40px;margin:5px;padding:auto;');
+rstBtn.textContent="RESET";
+btnHolder.appendChild(rstBtn);
+
+const setBtn=document.createElement('button');//adding button to our div
+setBtn.textContent="INSERT";//adding text content to our button
+setBtn.setAttribute('style','width:80px;height:40px;margin:5px;padding:auto;');//styling our button
+btnHolder.appendChild(setBtn);
+
+
+
+
+
+//adding event listener to run the entersquare function when the button is clicked
+setBtn.addEventListener('click',enterSquare);
+
+
