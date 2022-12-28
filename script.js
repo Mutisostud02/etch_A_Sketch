52cb4366759d54db2e57d3dd13c89b083b999250
Mutisostud02 <mutiso8395@gmail.com>
2022-12-29 01:33:07 +0300

    added new function to help reset the grid inorder to insert new grid squares updated the page to start with a default 16*16 grid added line to clear the initial 16*16 grid container re-arranged the structural flow of the code to have variables initialized at the top then the code follows

--- script.js
+++ script.js
@@ -3,33 +3,65 @@ document.body.appendChild(emptyDiv);
 emptyDiv.classList.add('empt');
 emptyDiv.setAttribute('style','width:100%;');
 
+const btnHolder=document.createElement('div');
+btnHolder.setAttribute('style','margin:10px 5vw 10px;height:100px;width:300px;margin:auto;padding:auto;display:inline-block;');
+document.body.insertBefore(btnHolder,emptyDiv);
 
-const container=document.createElement("div");//create a new div to contain the grid squares
+const rstBtn=document.createElement('button');
+rstBtn.setAttribute('style','width:80px;height:40px;margin:5px;padding:auto;');
+rstBtn.textContent="RESET";
+btnHolder.appendChild(rstBtn);
+
+const setBtn=document.createElement('button');//adding button to our div
+setBtn.textContent="INSERT";//adding text content to our button
+setBtn.setAttribute('style','width:80px;height:40px;margin:5px;padding:auto;');//styling our button
+btnHolder.appendChild(setBtn);
+
+
+let container=document.createElement("div");//create a new div to contain the grid squares
 container.classList.add('container');//gave the div a class
 
 document.body.appendChild(container);//adding our container to the div
 
 
 
-
-
-
-
-//function to take and return a number to be squared
-function enterSquare(){
-    let userInput=prompt('ENTER A SQUARE VALUE');
-    
-    noOfSquare(userInput);
-    
+container.setAttribute('style','margin:30px 30vw 30px;display:grid;grid-template-columns:repeat(16,10px);grid-template-rows:repeat(16,10px);');
+for(let i=0;i<256;i++){
+    const initSquare=document.createElement('div');
+    initSquare.classList.add('init');
+    initSquare.setAttribute('style','border:0.01px solid grey;');
+    container.appendChild(initSquare);
+    initSquare.addEventListener('mouseover',()=>{
+        changeColor(initSquare);
+    });
+   
 }
 
+function decoy(){
+   container.setAttribute('style','height:0;width:0;background-color;margin:-10px;');
+}
+function decoyTwo(){
+    newContainer.innerHTML='';
+}
+rstBtn.addEventListener('click',decoyTwo);
+//function to take and return a number to be squared
+function enterSquare(){
+    
+    let userInput=prompt('ENTER A SQUARE VALUE');
+    decoy();
+    
+    noOfSquare(userInput);
+}
+let newContainer=document.createElement('div');
+document.body.appendChild(newContainer);
+ 
+ 
 function noOfSquare(number){
-    container.setAttribute('style',`display:grid; grid-template-columns:repeat(${number},10px); grid-template-rows:repeat(${number},10px);`);
-    for(let i=0;i<(number*number);i++){
-        const divSquare=document.createElement('div');
-        divSquare.classList.add('box');
-        
-        container.appendChild(divSquare);
+    newContainer.setAttribute('style',`display:grid; grid-template-columns:repeat(${number},10px); grid-template-rows:repeat(${number},10px);`);
+  for(let i=0;i<(number*number);i++){
+       let divSquare=document.createElement('div');
+       divSquare.classList.add('box');
+        newContainer.appendChild(divSquare);
         divSquare.setAttribute('style','border:0.01px solid grey;');
         
             divSquare.addEventListener('mouseover',()=>{
@@ -41,7 +73,8 @@ function noOfSquare(number){
 }
 
 
-const divSquare=document.querySelectorAll('.box');//selecting our div squares
+
+
 
 //function to change element color
 function changeColor(element){
@@ -55,19 +88,6 @@ function returnColor(element){
     element.setAttribute('style','background-color:;border:1px black solid;')
 }
 
-const btnHolder=document.createElement('div');
-btnHolder.setAttribute('style','height:100px;width:300px;margin:auto;padding:auto;display:inline-block;');
-document.body.insertBefore(btnHolder,emptyDiv);
-
-const rstBtn=document.createElement('button');
-rstBtn.setAttribute('style','width:80px;height:40px;margin:5px;padding:auto;');
-rstBtn.textContent="RESET";
-btnHolder.appendChild(rstBtn);
-
-const setBtn=document.createElement('button');//adding button to our div
-setBtn.textContent="INSERT";//adding text content to our button
-setBtn.setAttribute('style','width:80px;height:40px;margin:5px;padding:auto;');//styling our button
-btnHolder.appendChild(setBtn);
 
 
 
